1a246c27e16eb9c9fd9695e06e3162e4bc41f0cb
junaid <junaidali8214@gmail.com>
2022-12-26 22:23:35 +0530

    first commit

---
+++ src/main/java/simplilearn/phase3project/App.java
@@ -0,0 +1,13 @@
+package simplilearn.phase3project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
